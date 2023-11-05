835e15cee2802f9e269363abe2ce54df601b49c0
Antritus <antt.graft@gmail.com>
2023-11-05 17:45:45 +0200

    Example

--- src/main/java/me/antritus/astraldupe/commands/staff/low/ChatCooldownCommand.java
+++ src/main/java/me/antritus/astraldupe/commands/staff/low/ChatCooldownCommand.java
@@ -5,6 +5,9 @@ import me.antritus.astraldupe.commands.AstralCommand;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
+import java.util.List;
+
 public class ChatCooldownCommand extends AstralCommand {
 	protected ChatCooldownCommand(AstralDupe main, @NotNull String name) {
 		super(main, name);
@@ -13,5 +16,10 @@ public class ChatCooldownCommand extends AstralCommand {
 	@Override
 	public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
 		return false;
+	}
+
+	@Override
+	public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
+		return Collections.emptyList();
 	}
 }
